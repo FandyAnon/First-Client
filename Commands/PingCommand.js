994dba46193274d641848c59f73801850699875a
Noodle | Matchatella <75095826+FandyAnon@users.noreply.github.com>
2021-02-14 05:49:44 +0800

    Matchatella

---
+++ Commands/PingCommand.js
@@ -0,0 +1,24 @@
+const { MessageEmbed } = require('discord.js')
+const Discord = require("discord.js");
+
+module.exports = {
+	name: 'Ping',
+	description: 'Network info',
+	aliases: ['ping','peng','pong','pung'],
+	cooldown: 2,
+	execute(client, message, args) {
+  
+  let start = Date.now();
+    let diff = (Date.now() - start).toLocaleString();
+    let API = client.ws.ping.toFixed();
+    let Matcha = new Discord.MessageEmbed()
+      .setAuthor("Network", client.user.displayAvatarURL({format: 'png', dynamic: true})+"?size=2048")
+      .setColor('fffaff')
+      .setFooter(`Development`)
+      .addField("Latency", `${diff}ms`, true)  
+      .addField("API", `${API}ms`, true)
+      .setTimestamp()
+      
+       message.channel.send(Matcha);
+     }
+ }
